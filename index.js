6f69bc9835db61be5b9f06e1d7ff97753e19f7fe
Simon Taylor <nizmox@gmail.com>
2017-02-16 09:56:28 +1100

    lint files

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const domain = 'https://www.kickstarter.com';
 
 function getDate($comment) {
   const datePath = '.comment-inner > .main > h3 > span.date > a > data';
-  return $comment.find(datePath).attr('data-value').replace(/\"/g, '');
+  return $comment.find(datePath).attr('data-value').replace(/"/g, '');
 }
 
 function getAuthor($comment) {
@@ -13,8 +13,8 @@ function getAuthor($comment) {
 }
 
 function getContent($, $comment) {
-  let contentArr = [];
-  $comment.find(".comment-inner > .main p").each(function() {
+  const contentArr = [];
+  $comment.find('.comment-inner > .main p').each(function each() {
     const text = $(this).text();
     contentArr.push(text);
   });
@@ -22,28 +22,28 @@ function getContent($, $comment) {
 }
 
 function getComments(url) {
-  return new Promise(function(resolve, reject) {
+  return new Promise((resolve, reject) => {
     jsdom.env({
       url,
-      scripts: ["http://code.jquery.com/jquery.js"],
-      done: function(err, window) {
+      scripts: ['http://code.jquery.com/jquery.js'],
+      done(err, window) {
         if (err) return reject(err);
 
         const $ = window.$;
         const comments = [];
-        $(".NS_comments__comment").each(function() {
+        $('.NS_comments__comment').each(function each() {
           const $comment = $(this);
 
           comments.push({
             author: getAuthor($comment),
             date: getDate($comment),
-            id:  $comment.attr('id'),
+            id: $comment.attr('id'),
             content: getContent($, $comment)
           });
         });
 
         const nextUrl = $('.older_comments').attr('href');
-        
+
         return resolve({ comments, nextUrl });
       }
     });
@@ -53,7 +53,7 @@ function getComments(url) {
 const path = '/projects/2074786394/vast-the-crystal-caverns-second-printing-with-mini/comments';
 
 getComments(`${domain}${path}`)
-.then(result => {
+.then((result) => {
   console.log('result', result);
 })
 .catch(err => console.log('ERROR:', err));
