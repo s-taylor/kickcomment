c3b8ef0b3d59a2c7c092d0ac7cf8e16d87b0d932
Simon Taylor <nizmox@gmail.com>
2017-02-16 09:26:49 +1100

    split into helper functions

--- index.js
+++ index.js
@@ -2,6 +2,25 @@ const jsdom = require('jsdom');
 
 const url = 'https://www.kickstarter.com/projects/2074786394/vast-the-crystal-caverns-second-printing-with-mini/comments';
 
+function getDate($comment) {
+  const datePath = '.comment-inner > .main > h3 > span.date > a > data';
+  return $comment.find(datePath).attr('data-value').replace(/\"/g, '');
+}
+
+function getAuthor($comment) {
+  const authorPath = '.comment-inner > .main > h3 > a.author';
+  return $comment.find(authorPath).text();
+}
+
+function getContent($, $comment) {
+  let contentArr = [];
+  $comment.find(".comment-inner > .main p").each(function() {
+    const text = $(this).text();
+    contentArr.push(text);
+  });
+  return contentArr.join('\n\n');
+}
+
 jsdom.env({
   url,
   scripts: ["http://code.jquery.com/jquery.js"],
@@ -11,24 +30,19 @@ jsdom.env({
     const $ = window.$;
     const comments = [];
     $(".NS_comments__comment").each(function() {
-      const comment = $(this);
-      const id = comment.attr('id');
-      const author = comment.find('.comment-inner > .main > h3 > a.author').text();
-      const date = comment.find('.comment-inner > .main > h3 > span.date > a > data').attr('data-value').replace(/\"/g, '');
+      const $comment = $(this);
 
-      let contentArr = [];
-      comment.find(".comment-inner > .main p").each(function() {
-        const text = $(this).text();
-        contentArr.push(text);
+      comments.push({
+        author: getAuthor($comment),
+        date: getDate($comment),
+        id:  $comment.attr('id'),
+        content: getContent($, $comment)
       });
-      const content = contentArr.join('\n\n');
-
-      comments.push({ author, date, id, content });
     });
 
     console.log(comments);
 
-    const nextUrl = window.$('.older_comments').attr('href');
+    const nextUrl = $('.older_comments').attr('href');
     console.log('---------------------------');
     console.log('nextUrl', nextUrl);
   }
