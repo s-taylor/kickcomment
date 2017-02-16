bf88609fa7e95b5045b8feb2322406d27ed36ecd
Simon Taylor <nizmox@gmail.com>
2017-02-16 13:09:01 +1100

    mock results with nock

--- test/index.test.js
+++ test/index.test.js
@@ -1,10 +1,21 @@
+const fs = require('fs');
+const nock = require('nock');
 const test = require('ava');
 const fetcher = require('../lib/fetcher');
+
+const path = '/projects/2074786394/vast-the-crystal-caverns-second-printing-with-mini/comments';
+
+test.before(() => {
+  const html = fs.readFileSync('./test/fixtures/example.html', 'utf-8');
+
+  nock('https://www.kickstarter.com')
+  .get(path)
+  .reply(200, html);
+});
 
 test('fetcher', t => {
   t.plan(3);
 
-  const path = '/projects/2074786394/vast-the-crystal-caverns-second-printing-with-mini/comments';
   return fetcher(path)
   .then(results => {
     t.is(typeof results, 'object');
