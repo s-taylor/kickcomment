aed4105acacd3e0a0131c44da751111bb2661990
Simon Taylor <nizmox@gmail.com>
2017-02-20 11:11:03 +1100

    fix lambda deployment

--- handler.js
+++ handler.js
@@ -1,35 +1,25 @@
+/* eslint-disable strict, no-var, vars-on-top, prefer-template */
+
 'use strict';
+
 // WARNING: must be node 4.3 compatible due to AWS lambda
-// no string interpolation
-// no object destructuring
-// no const???
 
 var getComments = require('./build/fetcher').getComments;
 
 module.exports.getComments = (event, context, callback) => {
-  if (!event.id) return callback('No (project) id found in query');
-  if (!event.name) return callback('No (project) name found in query');
+  // see https://serverless.com/framework/docs/providers/aws/events/apigateway/#default-request-templates
+  if (!event.query) return callback(new Error('[400] No query string provided'));
+  if (!event.query.id) return callback(new Error('[400] No (project) id found in query'));
+  if (!event.query.name) return callback(new Error('[400] No (project) name found in query'));
 
-  var path = '/projects/' + event.id + '/' + event.name + '/comments';
+  var path = '/projects/' + event.query.id + '/' + event.query.name + '/comments';
   // temporary limit until comments can be cached
   // retrieves last 4 pages (200 comments)
   var limit = 4;
 
   return getComments(path, limit)
-  .then(comments => {
-    var response = {
-      statusCode: 200,
-      body: JSON.stringify(comments)
-    };
-
-    callback(null, response);
-  })
-  .catch(err => {
-    var response = {
-      statusCode: 500,
-      body: JSON.stringify(err)
-    };
-
-    callback(response);
-  });
+  .then(comments => callback(null, comments))
+  .catch(err => callback(new Error('[500] ' + err.message)));
 };
+
+/* eslint-enable no-var, vars-on-top, prefer-template */
