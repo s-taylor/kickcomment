d312aec5b4f2e6b86dd9dfb96974d0295b80f744
Simon Taylor <nizmox@gmail.com>
2017-02-21 12:35:40 +1100

    add tests for handler

---
+++ test/handler.test.js
@@ -0,0 +1,55 @@
+const test = require('ava');
+const proxyquire = require('proxyquire');
+const sinon = require('sinon');
+
+function stubHandler(stub) {
+  return proxyquire('../handler.js', {
+    './build/fetcher': {
+      getComments: stub || function proxyFetch() { return Promise.resolve(); }
+    }
+  });
+}
+
+test.cb('getComments - errors when no query string', t => {
+  t.plan(1);
+  const handler = stubHandler();
+
+  handler.getComments({}, undefined, err => {
+    t.deepEqual(err, new Error('[400] No query string provided'));
+    t.end();
+  });
+});
+
+test.cb('getComments - errors when no project id', t => {
+  t.plan(1);
+  const handler = stubHandler();
+
+  handler.getComments({ query: {} }, undefined, err => {
+    t.deepEqual(err, new Error('[400] No (project) id found in query'));
+    t.end();
+  });
+});
+
+test.cb('getComments - errors when no project name', t => {
+  t.plan(1);
+  const handler = stubHandler();
+
+  handler.getComments({ query: { id: '1234567890' } }, undefined, err => {
+    t.deepEqual(err, new Error('[400] No (project) name found in query'));
+    t.end();
+  });
+});
+
+test.cb('getComments - must trigger fetcher with correct args', t => {
+  t.plan(3);
+  const stub = sinon.spy(() => Promise.resolve());
+  const handler = stubHandler(stub);
+
+  const query = { id: '1234567890', name: 'really-cool-project' };
+  handler.getComments({ query }, undefined, () => {
+    t.truthy(stub.calledOnce);
+    t.is(stub.args[0][0], `/projects/${query.id}/${query.name}/comments`);
+    t.is(stub.args[0][1], 4);
+    t.end();
+  });
+});
